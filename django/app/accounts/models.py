ce780bac99fac51dfb324ea7910e990dda4b39e4
Silvio Moreto <silviomoreto@gmail.com>
2013-09-27 23:10:14 -0300

    south and accounts model

--- django/app/accounts/models.py
+++ django/app/accounts/models.py
@@ -1,3 +1,15 @@
+# coding: utf-8
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'email'
+
+    facebook_id = models.CharField(_('facebook id'), max_length=20)
+    facebook_access_token = models.CharField(_('facebook access token'), max_length=320)
+
+    def __unicode__(self):
+        return self.email
--- django/app/settings/base.py
+++ django/app/settings/base.py
@@ -127,6 +127,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'south',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
