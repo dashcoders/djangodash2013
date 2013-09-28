8da0772e07de0ceb253d5d32e08977dac5ad59ec
Vinicius Cainelli <vinicius@viniciuscainelli.com>
2013-09-28 13:07:53 -0300

    added features in friend api

--- django/app/facebook/api/friend.py
+++ django/app/facebook/api/friend.py
@@ -44,7 +44,7 @@ class FriendResource(BaseResource):
         user = request.user
 
         query = user.fql({
-            'query_friends': 'SELECT name, pic_big, profile_url, uid, username FROM user WHERE uid2 IN (SELECT uid2 FROM friend WHERE uid1=me()))',
+            'query_friends': 'SELECT name, pic_big, profile_url, uid, username FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1=me())',
         })
 
         response = []
