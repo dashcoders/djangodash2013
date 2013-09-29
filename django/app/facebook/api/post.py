5893d1454e6b57edd2dcd70fc85d5b14f59181a5
Vinicius Cainelli <vinicius@viniciuscainelli.com>
2013-09-29 15:38:31 -0300

    facebook post

--- django/app/facebook/api/post.py
+++ django/app/facebook/api/post.py
@@ -10,43 +10,45 @@ class PostResource(BaseResource):
     class Meta(BaseResource.Meta):
         related_name = 'post'
 
-        in_friend_allowed_methods = ['get']
-        tagged_friend_allowed_methods = ['get']
+        tagged_allowed_methods = ['get']
+        in_timeline_allowed_methods = ['get']
 
     def base_urls(self):
         return [
             url(
-                r"^(?P<resource_name>%s)/in/(?P<friend_facebook_id>\d+)%s$" % (self._meta.resource_name, trailing_slash()),
-                self.wrap_view('dispatch_in_friend'),
-                name="api_dispatch_in_friend"
+                r"^(?P<resource_name>%s)/from/(?P<from_facebook_id>(\d+|me))/tagged/(?P<to_facebook_id>(\d+|me))%s$" % (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('dispatch_tagged'),
+                name="api_dispatch_tagged"
             ),
             url(
-                r"^(?P<resource_name>%s)/tagged/(?P<friend_facebook_id>\d+)%s$" % (self._meta.resource_name, trailing_slash()),
-                self.wrap_view('dispatch_tagged_friend'),
-                name="api_dispatch_tagged_friend"
+                r"^(?P<resource_name>%s)/from/(?P<from_facebook_id>(\d+|me))/in_timeline/(?P<to_facebook_id>(\d+|me))%s$" % (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('dispatch_in_timeline'),
+                name="api_dispatch_in_timeline"
             ),
         ]
 
-    def dispatch_in_friend(self, request, **kwargs):
-        return self.dispatch('in_friend', request, **kwargs)
+    def dispatch_tagged(self, request, **kwargs):
+        return self.dispatch('tagged', request, **kwargs)
 
-    def dispatch_tagged_friend(self, request, **kwargs):
-        return self.dispatch('tagged_friend', request, **kwargs)
+    def dispatch_in_timeline(self, request, **kwargs):
+        return self.dispatch('in_timeline', request, **kwargs)
 
-    def get_in_friend(self, request, **kwargs):
+    def get_tagged(self, request, **kwargs):
         user = request.user
-        friend_facebook_id = kwargs.get('friend_facebook_id')
+        from_facebook_id = kwargs.get('from_facebook_id')
+        to_facebook_id = kwargs.get('to_facebook_id')
 
         query_tags = user.fql(
             """
                 SELECT post_id, actor_id
                 FROM stream_tag
                 WHERE
-                    target_id = me()
-                    AND actor_id = '{friend_facebook_id}'
+                    target_id = {to_facebook_id}
+                    AND actor_id = {from_facebook_id}
                 LIMIT 10000
             """.format(
-                friend_facebook_id=friend_facebook_id,
+                from_facebook_id='me()' if from_facebook_id == 'me' else from_facebook_id,
+                to_facebook_id='me()' if to_facebook_id == 'me' else to_facebook_id,
             )
         )
 
@@ -66,7 +68,6 @@ class PostResource(BaseResource):
                         post_id IN ({post_ids})
                     LIMIT 10000
                 """.format(
-                    friend_facebook_id=friend_facebook_id,
                     post_ids=','.join(posts_ids),
                 )
             )
@@ -75,57 +76,25 @@ class PostResource(BaseResource):
 
         return self.create_response(request, posts)
 
-    def get_tagged_friend(self, request, **kwargs):
+    def get_in_timeline(self, request, **kwargs):
         user = request.user
-        friend_facebook_id = kwargs.get('friend_facebook_id')
+        from_facebook_id = kwargs.get('from_facebook_id')
+        to_facebook_id = kwargs.get('to_facebook_id')
 
-        query_tags = user.fql(
+        query_posts = user.fql(
             """
-                SELECT post_id, actor_id
-                FROM stream_tag
+                SELECT post_id, message, permalink, actor_id, created_time, attachment
+                FROM stream
                 WHERE
-                    target_id = '{friend_facebook_id}'
-                    AND actor_id = me()
+                    source_id = {from_facebook_id}
+                    AND actor_id = {to_facebook_id}
                 LIMIT 10000
             """.format(
-                friend_facebook_id=friend_facebook_id,
+                from_facebook_id='me()' if from_facebook_id == 'me' else from_facebook_id,
+                to_facebook_id='me()' if to_facebook_id == 'me' else to_facebook_id,
             )
         )
 
-        print """
-                SELECT post_id, actor_id
-                FROM stream_tag
-                WHERE
-                    target_id = '{friend_facebook_id}'
-                    AND actor_id = me()
-                LIMIT 10000
-            """.format(
-                friend_facebook_id=friend_facebook_id,
-            )
-
-        posts = []
-        tagged_posts = query_tags.get('data')
-
-        print tagged_posts
-
-        if tagged_posts:
-            posts_ids = []
-            for tagged_post in tagged_posts:
-                posts_ids.append("'{}_{}'".format(tagged_post.get('actor_id'), tagged_post.get('post_id')))
-
-            query_posts = user.fql(
-                """
-                    SELECT post_id, message, permalink, actor_id, created_time, attachment
-                    FROM stream
-                    WHERE
-                        post_id IN ({post_ids})
-                    LIMIT 10000
-                """.format(
-                    friend_facebook_id=friend_facebook_id,
-                    post_ids=','.join(posts_ids),
-                )
-            )
-
-            posts = query_posts.get('data')
+        posts = query_posts.get('data')
 
         return self.create_response(request, posts)
