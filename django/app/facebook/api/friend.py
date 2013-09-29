89e48468257f20bec3a8475e04ec451fc2cb533b
Vinicius Cainelli <vinicius@viniciuscainelli.com>
2013-09-29 13:24:14 -0300

    removed cache

--- django/app/facebook/api/friend.py
+++ django/app/facebook/api/friend.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 from django.conf.urls import url
-from django.core.cache import cache
 from tastypie.utils import trailing_slash
 from common.api import BaseResource
 
@@ -28,12 +27,6 @@ class FriendResource(BaseResource):
     def get_mutual(self, request, **kwargs):
         user = request.user
         friend_facebook_id = kwargs.get('friend_facebook_id')
-        cache_id = 'friend_list_user_mutual_%s_%s' % (user.facebook_id, friend_facebook_id)
-
-        cached_friends = cache.get(cache_id)
-
-        if cached_friends:
-            return self.create_response(request, cached_friends)
 
         query = user.fql({
             'query_friends': 'SELECT name, pic, pic_big, pic_small, profile_url, uid FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = \'{friend_facebook_id}\' AND uid2 IN (SELECT uid2 FROM friend WHERE uid1=me()))'.format(friend_facebook_id=friend_facebook_id),
@@ -44,18 +37,10 @@ class FriendResource(BaseResource):
         if query.get('data'):
             response = query.get('data')[0].get('fql_result_set' or [])
 
-        cache.set(cache_id, response, 24 * 60 * 60)
-
         return self.create_response(request, response)
 
     def get_list(self, request, **kwargs):
         user = request.user
-        cache_id = 'friend_list_user_%s' % user.facebook_id
-
-        cached_friends = cache.get(cache_id)
-
-        if cached_friends:
-            return self.create_response(request, cached_friends)
 
         query = user.fql({
             'query_friends': 'SELECT name, pic_big, pic_small, profile_url, uid FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1=me())',
@@ -65,7 +50,5 @@ class FriendResource(BaseResource):
 
         if query.get('data'):
             response = query.get('data')[0].get('fql_result_set' or [])
-
-        cache.set(cache_id, response, 24 * 60 * 60)
 
         return self.create_response(request, response)
