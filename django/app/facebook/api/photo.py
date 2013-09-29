3ae4e8f1d866f0b4df2f24d9daa198056db7638e
Vinicius Cainelli <vinicius@viniciuscainelli.com>
2013-09-29 18:23:07 -0300

    photos

--- django/app/facebook/api/photo.py
+++ django/app/facebook/api/photo.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 from tastypie.authentication import Authentication
 from django.conf.urls import url
-from django.core.cache import cache
 from common.api import BaseResource
 
 from tastypie.utils import trailing_slash
@@ -14,44 +13,15 @@ class PhotoResource(BaseResource):
         authentication = Authentication()
 
         with_friend_allowed_methods = ['get']
-        detail_allowed_methods = ['get', 'delete']
-        profile_allowed_methods = ['get']
-
-    def delete_detail(self, request, **kwargs):
-        user = request.user
-        response = user.graph_delete(url='/{0}/likes'.format(kwargs['pk']))
-        # response = user.graph_delete(url='/{0}/tags/{1}'.format(kwargs['pk'], user.facebook_id))
-        # not working, see issue: https://developers.facebook.com/bugs/122135101317762
-
-        return self.create_response(request, response)
 
     def base_urls(self):
         return [
-            url(r"^(?P<resource_name>%s)/(?P<pk>\d+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
             url(
                 r"^(?P<resource_name>%s)/with/(?P<friend_facebook_id>\d{1,32})%s$" % (self._meta.resource_name, trailing_slash()),
                 self.wrap_view('dispatch_with_friend'),
                 name="api_dispatch_with_friend",
             ),
-            url(
-                r"^(?P<resource_name>%s)/profile/(?P<width>\d+)/(?P<height>\d+)%s$" % (self._meta.resource_name, trailing_slash()),
-                self.wrap_view('dispatch_profile'),
-                name="api_dispatch_profile",
-            ),
         ]
-
-    def dispatch_profile(self, request, **kwargs):
-        return self.dispatch('profile', request, **kwargs)
-
-    def get_profile(self, request, **kwargs):
-        user = request.user
-        width = kwargs.get('width')
-        height = kwargs.get('height')
-
-        response = user.graph_get('/me/picture/', params={'type': 'large', 'redirect': False})
-
-        return self.create_response(request, response.get('data'))
-
 
     def dispatch_with_friend(self, request, **kwargs):
         return self.dispatch('with_friend', request, **kwargs)
@@ -59,12 +29,6 @@ class PhotoResource(BaseResource):
     def get_with_friend(self, request, **kwargs):
         friend_facebook_id = kwargs.get('friend_facebook_id')
         user = request.user
-        cache_id = 'photo_list_user_mutual_%s_%s' % (user.facebook_id, friend_facebook_id)
-
-        cached_photos = cache.get(cache_id)
-
-        if cached_photos:
-            return self.create_response(request, cached_photos)
 
         response = user.fql({
             'query1_tags': 'SELECT pid, xcoord, ycoord FROM photo_tag WHERE pid IN (SELECT pid FROM photo WHERE owner = \'{friend_facebook_id}\') AND subject = me()'.format(friend_facebook_id=friend_facebook_id),
@@ -81,7 +45,5 @@ class PhotoResource(BaseResource):
                 if result.get('pid') not in photos:
                     photos[result.get('pid')] = {}
                 photos[result.get('pid')].update(result)
-
-        cache.set(cache_id, photos.values(), 24 * 60 * 60)
 
         return self.create_response(request, photos.values())
