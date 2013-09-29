c976b72ed3fac95ec868ec3d302f5d6ec91cf256
Silvio Moreto <silviomoreto@gmail.com>
2013-09-29 15:29:32 -0300

    get comments from other timeline

--- django/app/facebook/api/comment.py
+++ django/app/facebook/api/comment.py
@@ -25,18 +25,23 @@ class CommentResource(BaseResource):
         user = request.user
         friend_facebook_id = kwargs.get('friend_facebook_id')
 
-        response = user.fql(
-            """
-                SELECT post_id, text FROM comment WHERE post_id IN (
-                    SELECT post_id FROM stream WHERE source_id = me() LIMIT 100000000
-                ) AND fromid = '{friend_facebook_id}' LIMIT 100000
-            """.format(friend_facebook_id=friend_facebook_id)
-        )
+        query1 = 'SELECT post_id, text FROM comment WHERE post_id IN (SELECT post_id FROM stream WHERE source_id = me() LIMIT 100000000) AND fromid = \'{friend_facebook_id}\' LIMIT 100000'.format(friend_facebook_id=friend_facebook_id)
+        query2 = 'SELECT post_id, text FROM comment WHERE post_id IN (SELECT post_id FROM stream WHERE source_id = \'{friend_facebook_id}\' LIMIT 100000000) AND fromid = me() LIMIT 100000'.format(friend_facebook_id=friend_facebook_id)
 
+        response = user.fql({
+            'query_source1': query1,
+            'query_source2': query2,
+        })
+
+        comments = {}
         facebook_post = 'https://www.facebook.com/{0}/posts/{1}'
 
-        for post in response.get('data'):
-            user_id, post_id = post.get('post_id').split('_')
-            post['permalink'] = facebook_post.format(user_id, post_id)
+        for result in response.get('data'):
+            for post in result.get('fql_result_set'):
+                if post.get('post_id') not in comments:
+                    comments[post.get('post_id')] = {}
+                user_id, post_id = post.get('post_id').split('_')
+                post['permalink'] = facebook_post.format(user_id, post_id)
+                comments[post.get('post_id')].update(post)
 
-        return self.create_response(request, response.get('data'))
+        return self.create_response(request, comments.values())
